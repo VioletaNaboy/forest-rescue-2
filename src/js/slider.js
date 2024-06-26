b2e0250e237ec93eff5925d7e766f0d4513a8879
parasolya <o.shevchuk@gmail.com>
2024-06-26 19:06:48 +0300

    reviews done

--- src/js/slider.js
+++ src/js/slider.js
@@ -6,31 +6,68 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 import slidesImages from '../data/slides.json' assert { type: 'json' };
+import reviews from '../data/reviews.json' assert { type: 'json' };
 
-const swiperWrapper = document.querySelector('.swiper-wrapper');
 
-function createImageSlides(container, slides) {
+function createImageSlides(wrapper, slides) { 
   slides.forEach(slide => {
     const slideDiv = document.createElement('div');
-    slideDiv.className = 'swiper-slide';
+    slideDiv.className = 'swiper-slide swiper-slide__gallery';
     const img = document.createElement('img');
     img.className = 'slide__img';
     img.src = slide.src;
     img.alt = slide.alt;
 
     slideDiv.appendChild(img);
-    swiperWrapper.appendChild(slideDiv);
+    wrapper.appendChild(slideDiv);
   });
 }
 
-function initializeSwiper(slidesConteiner, slides, createSlidesFunc) {
-  const slidesWrapper = document.querySelector(
-    `${slidesConteiner} .swiper-wrapper`
-  );
+function createReviewsItems(wrapper, slides) { 
+  slides.forEach((item, index) => {
+    const li = document.createElement('div');
+    li.className = `swiper-slide swiper-slide__reviews reviews-li reviews-li-${index}`; 
 
-  createSlidesFunc(slidesConteiner, slides);
+    const box = document.createElement('div');
+    box.className = 'reviews-box';
 
-  return new Swiper(slidesConteiner, {
+    const description = document.createElement('p');
+    description.textContent = item.description;
+    description.className = 'reviews-description';
+
+    const divUser = document.createElement('div');
+    divUser.className = 'reviews-user';
+
+    const avatarBox = document.createElement('div');
+    avatarBox.className = 'reviews-avatar__box';
+
+    const avatar = document.createElement('img');
+    avatar.className = 'reviews-avatar';
+    avatar.src = item.urlAvatar;
+    avatar.alt = item.altAvatar;
+
+    const name = document.createElement('p');
+    name.textContent = item.name;
+
+    avatarBox.appendChild(avatar);
+    divUser.appendChild(avatarBox);
+    divUser.appendChild(name);
+
+    box.appendChild(description);
+    box.appendChild(divUser);
+
+    li.appendChild(box);
+    wrapper.appendChild(li); 
+  });
+}
+
+function initializeSwiper(containerSelector, slides, createSlidesFunc) {
+  const swiperContainer = document.querySelector(containerSelector); 
+  const wrapper = swiperContainer.querySelector('.swiper-wrapper'); 
+
+  createSlidesFunc(wrapper, slides); 
+
+  return new Swiper(containerSelector, {
     modules: [Navigation, Pagination],
     pagination: {
       el: '.swiper-pagination',
@@ -39,12 +76,10 @@ function initializeSwiper(slidesConteiner, slides, createSlidesFunc) {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
     breakpoints: {
       320: {
         spaceBetween: 16,
       },
-
       1440: {
         slidesPerView: 4,
         spaceBetween: 69,
@@ -55,4 +90,6 @@ function initializeSwiper(slidesConteiner, slides, createSlidesFunc) {
 
 document.addEventListener('DOMContentLoaded', () => {
   initializeSwiper('.swiper1', slidesImages, createImageSlides);
+  initializeSwiper('.swiper2', reviews, createReviewsItems);
 });
+
