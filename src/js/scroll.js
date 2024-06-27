96d1082bda4931de0307daafb6b9c27dfdebe9a5
VioletaNaboy <121202853+VioletaNaboy@users.noreply.github.com>
2024-06-27 18:34:51 +0300

    fixed_scroll_aria-label

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,15 +1,31 @@
-document.querySelectorAll('.nav-link').forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const targetId = this.getAttribute('href').substring(2);
-    const targetElement = document.getElementById(targetId);
-    const headerOffset = document.querySelector('.header').offsetHeight;
-    const elementPosition = targetElement.getBoundingClientRect().top;
-    const offsetPosition = elementPosition - headerOffset + window.pageYOffset;
-
-    window.scrollTo({
-      top: offsetPosition,
-      behavior: 'smooth',
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute('href').substring(2);
+      scrollToSection(targetId);
     });
   });
+
+  if (window.location.hash) {
+    const targetId = window.location.hash.substring(1);
+    setTimeout(() => {
+      scrollToSection(targetId);
+    }, 100);
+  }
+
+  function scrollToSection(targetId) {
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      const headerOffset = document.querySelector('.header').offsetHeight;
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition =
+        elementPosition - headerOffset + window.pageYOffset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth',
+      });
+    }
+  }
 });
